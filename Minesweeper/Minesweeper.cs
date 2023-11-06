557895d7ab03aa3ea47e3a8aeeaa7f5e4e9d1ce5
Arvid Larsson <arvid94@gmail.com>
2023-11-06 20:33:14 +0100

    feat: add instructions

--- Minesweeper/Minesweeper.cs
+++ Minesweeper/Minesweeper.cs
@@ -5,18 +5,20 @@ class Minesweeper
     static void Main()
     {
         Console.WriteLine("Welcome to minesweeper");
+        Console.WriteLine("Use arrows to control cursor");
+        Console.WriteLine("Press Enter to uncover tile");
+
         var field = new Minefield(9, 9);
-
-        field.InitializeRandomBombs(5);
-
         var controller = new ConsoleGameController(field);
         var display = new ConsoleGameDisplay(field, controller);
+        field.InitializeRandomBombs(10);
 
         while (field.GetGameState() == GameState.Playing)
         {
             display.DrawGame();
             controller.HandleUserInput();
         }
+
         display.DrawGame();
         if (field.GetGameState() == GameState.Won)
         {
